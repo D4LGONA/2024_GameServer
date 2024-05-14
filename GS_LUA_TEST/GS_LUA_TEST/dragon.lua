bbc8c04eb8f086e602af726e57093fff2a74463e
D4LGONA(samsung) <laff3y2297@gmail.com>
2024-05-14 16:16:25 +0900

    c언어로 작성된 함수 lua에서 호출하기

--- GS_LUA_TEST/GS_LUA_TEST/dragon.lua
+++ GS_LUA_TEST/GS_LUA_TEST/dragon.lua
@@ -3,6 +3,13 @@ local a;
 a = 10
 return x+a;
 end
+
+function event_add(a, b)
+local c
+c = callC_add(a, b)
+return c
+end
+
 --here we see a simple function but it could be very complex,
 --even check for user input
 pos_x = 6; -- comments in a config file can be handy
--- GS_LUA_TEST/GS_LUA_TEST/main.cpp
+++ GS_LUA_TEST/GS_LUA_TEST/main.cpp
@@ -3,6 +3,16 @@
 
 #pragma comment (lib, "lua54.lib")
 
+int LUAapi_add(lua_State* L)
+{
+	int a = lua_tonumber(L, -1);
+	int b = lua_tonumber(L, -2);
+	lua_pop(L, 3); // 3개 꺼내는 것
+	int c = a + b;
+	lua_pushnumber(L, c);
+	return 1; // 푸시한것의 개수
+}
+
 int main()
 {
 	//const char* program = "print \"Hello from Lua\""; // 코딩을 파일로 해도 되고 이렇게 해도 됨
@@ -27,12 +37,21 @@ int main()
 	//std::cout << "pos_x: " << pos_x << ", pos_y: " << pos_y << std::endl;
 	//// 게임서버에서 알아서 활용하도록 ...
 
-	lua_getglobal(L, "plustwo"); // 함수이름을 적어주도록
-	lua_pushnumber(L, 10); // plustwo 함수의 파라미터
-	lua_pcall(L, 1, 1, 0); // 파라미터 1개, 리턴값 1개 -> 스택의 맨 위에 저장
+	lua_register(L, "callC_add", LUAapi_add);
+
+	lua_getglobal(L, "event_add"); // 함수이름을 적어주도록
+	lua_pushnumber(L, 10);
+	lua_pushnumber(L, 20);
+	int ret2 = lua_pcall(L, 2, 1, 0); // 파라미터 1개, 리턴값 1개 -> 스택의 맨 위에 저장
 	int res = lua_tonumber(L, -1);
-	lua_pop(L, 1);
-	std::cout << "result: " << res << std::endl;
+	if (0 != ret2) {
+		std::cout << "error: " << lua_tostring(L, -1); // 에러를 L 머신에서 1개 뽑아라
+		lua_pop(L, 1);
+	}
+	else {
+		lua_pop(L, 1);
+		std::cout << "result: " << res << std::endl;
+	}
 
 	lua_close(L); // delete 무조건 해줘야 함
 }
