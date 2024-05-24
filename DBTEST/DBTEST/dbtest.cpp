9a94162c97f7fc62148fe81e1223a79be3ef4d0e
D4LGONA(samsung) <laff3y2297@gmail.com>
2024-05-24 15:40:54 +0900

    데이터 읽어오기 실습

--- DBTEST/DBTEST/dbtest.cpp
+++ DBTEST/DBTEST/dbtest.cpp
@@ -1,24 +1,32 @@
-// SQLConnect_ref.cpp  
-// compile with: odbc32.lib -> 알아서 넣어 줌
+// SQLBindCol_ref.cpp  
+// compile with: odbc32.lib  
 #include <windows.h>  
+#include <stdio.h>  
+
+#define UNICODE  
 #include <sqlext.h>  
-#include <iostream>
+
+#define NAME_LEN 50  
+#define PHONE_LEN 60
+
+void show_error() {
+    printf("error\n");
+}
 
 int main() {
     SQLHENV henv;
     SQLHDBC hdbc;
-    SQLHSTMT hstmt;
+    SQLHSTMT hstmt = 0;
     SQLRETURN retcode;
-
-    SQLCHAR* OutConnStr = (SQLCHAR*)malloc(255);
-    SQLSMALLINT* OutConnStrLen = (SQLSMALLINT*)malloc(255);
+    SQLWCHAR szName[NAME_LEN], szPhone[PHONE_LEN], sCustID[NAME_LEN];
+    SQLLEN cbName = 0, cbCustID = 0, cbPhone = 0;
 
     // Allocate environment handle  
     retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);
 
     // Set the ODBC version environment attribute  
     if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
-        retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (void*)SQL_OV_ODBC3, 0);
+        retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER*)SQL_OV_ODBC3, 0);
 
         // Allocate connection handle  
         if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
@@ -28,20 +36,44 @@ int main() {
             if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
                 SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
 
-                // Connect to data source -> 요기! ODBC 이름을 줘야 함
+                // Connect to data source  
                 retcode = SQLConnect(hdbc, (SQLWCHAR*)L"2024_TF", SQL_NTS, (SQLWCHAR*)NULL, 0, NULL, 0);
 
                 // Allocate statement handle  
                 if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
                     retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
 
+                    retcode = SQLExecDirect(hstmt, (SQLWCHAR*)L"SELECT user_id, user_name, user_level FROM user_table", SQL_NTS); // 쿼리는 핵심만 간단하게
+                    if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+
+                        // Bind columns 1, 2, and 3  
+                        retcode = SQLBindCol(hstmt, 1, SQL_C_WCHAR, &sCustID, 100, &cbCustID);
+                        retcode = SQLBindCol(hstmt, 2, SQL_C_WCHAR, szName, NAME_LEN, &cbName);
+                        retcode = SQLBindCol(hstmt, 3, SQL_C_WCHAR, szPhone, PHONE_LEN, &cbPhone);
+
+                        // Fetch and print each row of data. On an error, display a message and exit.  
+                        for (int i = 0; ; i++) {
+                            retcode = SQLFetch(hstmt);
+                            if (retcode == SQL_ERROR || retcode == SQL_SUCCESS_WITH_INFO)
+                                show_error();
+                            if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO)
+                            {
+                                //replace wprintf with printf
+                                //%S with %ls
+                                //warning C4477: 'wprintf' : format string '%S' requires an argument of type 'char *'
+                                //but variadic argument 2 has type 'SQLWCHAR *'
+                                //wprintf(L"%d: %S %S %S\n", i + 1, sCustID, szName, szPhone);  
+                                printf("%d: %ls %ls %ls\n", i + 1, sCustID, szName, szPhone);
+                            }
+                            else
+                                break;
+                        }
+                    }
+
                     // Process data  
                     if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+                        SQLCancel(hstmt);
                         SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
-
-                        // 성공한 경우
-                        std::cout << "Success" << std::endl;
-
                     }
 
                     SQLDisconnect(hdbc);
